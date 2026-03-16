02e2f3ab9936230dc8b580f146f5a39d6442bc22
GABEHACKER33 <gabriel.oli.ren.micro@outlook.es>
2026-03-16 08:39:32 +0100

    Create scramjet.config.js

---
+++ public/scram/scramjet.config.js
@@ -0,0 +1,11 @@
+self.__scramjet$config = {
+  prefix: "/scram/service/",
+  codec: "plain",
+  files: {
+    wasm:   "/scram/scramjet.wasm.js",
+    worker: "/scram/scramjet.worker.js",
+    client: "/scram/scramjet.client.js",
+    shared: "/scram/scramjet.shared.js",
+    sync:   "/scram/scramjet.sync.js",
+  },
+};
